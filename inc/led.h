c8a197de062c4eb72c74b9e2ddcf5daf8bb6ff9b
Paul Smith <paul@elandesign.co.uk>
2024-07-11 22:12:33 +0100

    Change LED behaviour
    LEDs should blink when a CAN frame is received or sent

--- inc/led.h
+++ inc/led.h
@@ -2,23 +2,20 @@
 #define _LED_H
 
 
-#define LED_BLUE_Pin GPIO_PIN_1
-#define LED_BLUE_Port GPIOB
-#define LED_BLUE LED_BLUE_Port , LED_BLUE_Pin
+#define LED_RX_Pin GPIO_PIN_1
+#define LED_RX_Port GPIOB
+#define LED_RX LED_RX_Port , LED_RX_Pin
 
-#define LED_GREEN_Pin GPIO_PIN_0
-#define LED_GREEN_Port GPIOB
-#define LED_GREEN LED_GREEN_Port , LED_GREEN_Pin
+#define LED_TX_Pin GPIO_PIN_0
+#define LED_TX_Port GPIOB
+#define LED_TX LED_TX_Port , LED_TX_Pin
 
 
 
 #define LED_DURATION 25 
 
 void led_init();
-void led_blue_blink(uint8_t numblinks);
-void led_green_on(void);
-void led_green_off(void);
-void led_blue_on(void);
-void led_process(void);
+void led_tx_blink(uint8_t numblinks);
+void led_rx_blink(uint8_t numblinks);
 
 #endif
--- src/led.c
+++ src/led.c
@@ -6,13 +6,6 @@
 #include "led.h"
 
 
-// Private variables
-static uint32_t led_blue_laston = 0;
-static uint32_t led_green_laston = 0;
-static uint32_t led_blue_lastoff = 0;
-static uint32_t led_green_lastoff = 0;
-
-
 // Initialize LED GPIOs
 void led_init()
 {
@@ -25,76 +18,32 @@ void led_init()
     GPIO_InitStruct.Alternate = 0;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    HAL_GPIO_WritePin(LED_GREEN, 1); 
+    HAL_GPIO_WritePin(LED_TX, 1);
+    HAL_GPIO_WritePin(LED_RX, 1);
 }
-
-
-// Turn green LED on
-void led_green_on(void)
-{
-	// Make sure the LED has been off for at least LED_DURATION before turning on again
-	// This prevents a solid status LED on a busy canbus
-	if(led_green_laston == 0 && HAL_GetTick() - led_green_lastoff > LED_DURATION)
-	{
-        // Invert LED
-		HAL_GPIO_WritePin(LED_GREEN, 0);
-		led_green_laston = HAL_GetTick();
-	}
-}
-
-
-// Turn green LED on
-void led_green_off(void)
-{
-	HAL_GPIO_WritePin(LED_GREEN, 0);
-}
-
 
 // Blink blue LED (blocking)
-void led_blue_blink(uint8_t numblinks)
+void led_tx_blink(uint8_t numblinks)
 {
 	uint8_t i;
 	for(i=0; i<numblinks; i++)
 	{
-		HAL_GPIO_WritePin(LED_BLUE, 1);
+		HAL_GPIO_WritePin(LED_TX, 0);
 		HAL_Delay(100);
-		HAL_GPIO_WritePin(LED_BLUE, 0);
+		HAL_GPIO_WritePin(LED_TX, 1);
 		HAL_Delay(100);
 	}
 }
 
-
-// Attempt to turn on status LED
-void led_blue_on(void)
+// Blink blue LED (blocking)
+void led_rx_blink(uint8_t numblinks)
 {
-	// Make sure the LED has been off for at least LED_DURATION before turning on again
-	// This prevents a solid status LED on a busy canbus
-	if(led_blue_laston == 0 && HAL_GetTick() - led_blue_lastoff > LED_DURATION)
+	uint8_t i;
+	for(i=0; i<numblinks; i++)
 	{
-		HAL_GPIO_WritePin(LED_BLUE, 1);
-		led_blue_laston = HAL_GetTick();
+		HAL_GPIO_WritePin(LED_RX, 0);
+		HAL_Delay(100);
+		HAL_GPIO_WritePin(LED_RX, 1);
+		HAL_Delay(100);
 	}
 }
-
-
-// Process time-based LED events
-void led_process(void)
-{
-	// If LED has been on for long enough, turn it off
-	if(led_blue_laston > 0 && HAL_GetTick() - led_blue_laston > LED_DURATION)
-	{
-		HAL_GPIO_WritePin(LED_BLUE, 0);
-		led_blue_laston = 0;
-		led_blue_lastoff = HAL_GetTick();
-	}
-
-	// If LED has been on for long enough, turn it off
-	if(led_green_laston > 0 && HAL_GetTick() - led_green_laston > LED_DURATION)
-	{
-        // Invert LED
-		HAL_GPIO_WritePin(LED_GREEN, 1);
-		led_green_laston = 0;
-		led_green_lastoff = HAL_GetTick();
-	}
-}
-
